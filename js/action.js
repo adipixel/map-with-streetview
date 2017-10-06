c05b7a1271c0b8afae404f9feac32ccf0e49a50e
adipixel <me@adityamhamunkar.com>
2017-10-06 13:47:11 -0400

    Added markers

--- js/action.js
+++ js/action.js
@@ -3,10 +3,27 @@ var map;
 
 // TODO: Complete the following function to initialize the map
 function initMap() {
-	// TODO: use a constructor to create a new map JS object. You can use the coordinates
-	map = new google.maps.Map(document.getElementById("map"),{
-		center: {lat: 40.7413549, lng: -73.99802439999996},
-		zoom: 13
-	});
-	// we used, 40.7413549, -73.99802439999996 or your own!
+  var myLatLng = {lat: 40.7413549, lng: -73.99802439999996};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Google\'s New York City Office',
+    draggable: true,
+    animation: google.maps.Animation.DROP
+  });
+	marker.addListener('click', toggleBounce);
+}
+
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
 }
